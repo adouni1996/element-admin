a5f758c82de396cb6e334b7e4e99e8d61c7098eb
fjc <>
2020-06-19 17:42:40 +0800

    用户列表

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -31,13 +31,14 @@
 					      background-color="#545c64"
 					      text-color="#fff"
 					      active-text-color="#ffd04b"
-								unique-opened = true>				
+								unique-opened = true
+								router="true">				
 								<el-submenu index="1">
 								  <template slot="title">
 								    <i class="el-icon-user"></i>
 								    <span>用户管理</span>
 								  </template>				
-								    <el-menu-item index="1-1">
+								    <el-menu-item index="users">
 											<i class="el-icon-notebook-1"></i>
 											<span>用户列表</span>
 										</el-menu-item>			 
@@ -98,7 +99,10 @@
 					  </el-col>
 					</el-row>
 				</el-aside>
-		    <el-main class='main'>Main</el-main>
+		    <el-main class='main'>
+					
+					<router-view></router-view>
+				</el-main>
 		  </el-container>
 		</el-container>
 	</div>
